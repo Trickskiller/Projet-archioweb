55a486b84b01bc0cb37d4596b0b88350f612b2f2
Andy Kanga <55740993+Trickskiller@users.noreply.github.com>
2023-11-28 15:01:58 +0100

    Modification de la route vehicules

--- routes/vehicules.js
+++ routes/vehicules.js
@@ -26,8 +26,9 @@ router.post("/", authenticate, async (req, res) => {
 
     console.log(newVehicule);
     newVehicule.userId = req.currentUserId;
+    console.log(req.currentUserId);
     await newVehicule.save();
-    res.status(201).send("Véhicule enregistré avec succès.", newVehicule);
+    res.status(201).send("Véhicule enregistré avec succès.");
   } catch (error) {
     res.status(500).json({ error: "Erreur de création de véhicule" });
   }
@@ -36,33 +37,38 @@ router.post("/", authenticate, async (req, res) => {
 // Route de mise à jour d'un véhicule par son ID, avec vérification du propriétaire
 router.put("/:vehiculeId", authenticate, async (req, res) => {
   try {
-    const _id = req.params.vehiculeId;
+    console.log("Mise à jour du véhicule", req.params.vehiculeId);
+    const vehiculeId = req.params.vehiculeId;
     const updateData = req.body;
 
-    // Rechercher le véhicule par ID
     const vehicule = await Vehicule.findById(vehiculeId);
+    console.log("Véhicule trouvé:", vehicule);
 
-    // Vérifier si le véhicule existe
     if (!vehicule) {
       return res.status(404).json({ error: "Véhicule non trouvé" });
     }
 
-    // Vérifier si l'utilisateur authentifié est le propriétaire du véhicule
-    if (userId !== req.currentUserId) {
+    console.log(
+      "Vérification du propriétaire",
+      vehicule.userId,
+      req.currentUserId
+    );
+    if (vehicule.userId.toString() !== req.currentUserId) {
       return res.status(403).json({ error: "Action non autorisée" });
     }
 
-    // Mise à jour du véhicule
     const updatedVehicule = await Vehicule.findByIdAndUpdate(
       vehiculeId,
       updateData,
       { new: true }
     );
+    console.log("Véhicule mis à jour", updatedVehicule);
 
     res
       .status(200)
       .json({ message: "Véhicule mis à jour avec succès", updatedVehicule });
   } catch (error) {
+    console.error("Erreur lors de la mise à jour:", error);
     res
       .status(500)
       .json({ error: "Erreur lors de la mise à jour du véhicule" });
